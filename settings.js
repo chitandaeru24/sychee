c7df7124ef21162d558354c5d7a1ed4c96dc2af3
chitandaeru24 <chitandaeru2408@gmail.com>
2025-04-17 03:36:11 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,12 +7,12 @@ const chalk = require('chalk');
  */
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 //settings owner di ./setown.js
-global.packname = 'SychBotz'
-global.owner = ["62882008702155"]
-global.botnum = ["6287862997267"]
-global.author = 'ydaa'
-global.owname = 'mzyda'
-global.botname = 'SYCHY BOTz'
+global.packname = 'ChitandaBot'
+global.owner = ["6287783834137"]
+global.botnum = ["6287783834137"]
+global.author = 'haedar'
+global.owname = 'haedar'
+global.botname = 'CHITANDA ERU BOTz'
 global.themeemoji = '🪀'
 global.f = '> '
 global.n = '`'
